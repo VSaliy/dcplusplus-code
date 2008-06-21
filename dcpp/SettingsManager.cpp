92ace80f117225618c80ee5bd6b949a457bf0d95
Jacek Sieka <arnetheduck@gmail.com>
2008-06-21 22:04:59 +0200

    upload speed

--- dcpp/SettingsManager.cpp
+++ dcpp/SettingsManager.cpp
@@ -104,6 +104,7 @@ SettingsManager::SettingsManager()
 	connectionSpeeds.push_back("20");
 	connectionSpeeds.push_back("50");
 	connectionSpeeds.push_back("100");
+	connectionSpeeds.push_back("1000");
 
 	for(int i=0; i<SETTINGS_LAST; i++)
 		isSet[i] = false;
