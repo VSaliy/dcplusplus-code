bb3961896598c1f2c7866d13167124501754b527
poy <poy@123gen.com>
2008-07-03 02:23:22 +0200

    leftover

--- win32/MainWindow.h
+++ win32/MainWindow.h
@@ -141,7 +141,6 @@ private:
 
 	// User actions
 	void handleExit();
-	void handleOpenWindow(unsigned id);
 	void handleFavHubsDropDown(const dwt::ScreenCoordinate& pt);
 	void handleQuickConnect();
 	void handleSettings();
