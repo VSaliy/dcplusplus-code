7d25319be168a4c3b66cda96942aa03ae47585cd
poy <poy@123gen.com>
2009-03-19 23:26:48 +0100

    help: include files with SSI tags, used for user-related commands as a start

--- help/gen_compile.py
+++ help/gen_compile.py
@@ -1,11 +1,12 @@
 def gen_compile(target, source, env):
+	from cgi import escape
 	f_target = open(str(target[0]), "wb")
 	f_source = open(str(source[0]), "rb")
 	f_template = open(str(source[1]), "rb")
 	f_target.write(f_template.read().replace("<!-- contents -->", """
 <h1>Instructions to compile DC++</h1>
 <pre>
-""" + f_source.read() + "\n</pre>", 1))
+""" + escape(f_source.read()) + "\n</pre>", 1))
 	f_target.close()
 	f_source.close()
 	f_template.close()
