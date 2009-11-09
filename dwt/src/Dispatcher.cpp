da9b7d790b9912abd9226360812ca012b53a50d4
poy <poy@123gen.com>
2009-11-09 19:00:24 +0100

    fix widget attach and clean up remnants of the previous window proc policies

--- dwt/src/Dispatcher.cpp
+++ dwt/src/Dispatcher.cpp
@@ -36,252 +36,132 @@
 
 namespace dwt {
 
-template<typename Policy>
-class WindowProc {
-public:
-	typedef WindowProc<Policy> PolicyType;
+LRESULT CALLBACK WindowProc::initProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+	MSG msg = { hwnd, uMsg, wParam, lParam };
 
-	static LRESULT CALLBACK initProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-		MSG msg = { hwnd, uMsg, wParam, lParam };
+	Widget* w = getInitWidget(msg);
+	if(w) {
+		w->setHandle(hwnd);
 
-		Widget* w = Policy::getInitWidget(msg);
-		if(w) {
-			w->setHandle(hwnd);
-
-			// Set the normal dispatcher
-			::SetWindowLongPtr(hwnd, GWLP_WNDPROC, (LONG_PTR)wndProc);
-
-			return wndProc(hwnd, uMsg, wParam, lParam);
-		}
-
-		return Policy::returnUnknown(msg);
+		return wndProc(hwnd, uMsg, wParam, lParam);
 	}
 
-	static LRESULT CALLBACK wndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-		// We dispatch certain messages back to the child widget, so that their
-		// potential callbacks will die along with the child when the time comes
-		HWND handler = getHandler(hwnd, uMsg, wParam, lParam);
+	return returnUnknown(msg);
+}
 
-		MSG msg = { hwnd, uMsg, wParam, lParam };
+LRESULT CALLBACK WindowProc::wndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+	MSG msg = { hwnd, uMsg, wParam, lParam };
 
-		// Try to get the this pointer
-		Widget* w = hwnd_cast<Widget*>(handler);
+	// We dispatch certain messages back to the child widget, so that their
+	// potential callbacks will die along with the child when the time comes
+	HWND handler = getHandler(msg);
 
-		if(w) {
-			if(uMsg == WM_NCDESTROY) {
-				w->kill();
-				return Policy::returnDestroyed(msg);
-			}
+	// Try to get the this pointer
+	Widget* w = hwnd_cast<Widget*>(handler);
 
-			LRESULT res = 0;
-			if(w->handleMessage(msg, res)) {
-				return Policy::returnHandled(res, msg);
-			}
+	if(w) {
+		if(uMsg == WM_NCDESTROY) {
+			w->kill();
+			return returnUnknown(msg);
 		}
 
-		if(handler != hwnd) {
-			w = hwnd_cast<Widget*>(hwnd);
+		LRESULT res = 0;
+		if(w->handleMessage(msg, res)) {
+			return res;
 		}
-
-		// If this fails there's something wrong
-		dwtassert(w, "Expected to get a pointer to a widget - something's wrong");
-
-		if(!w) {
-			return Policy::returnUnknown(msg);
-		}
-
-	    return Policy::returnUnhandled(w, msg);
 	}
 
-private:
-	static HWND getHandler(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-		HWND handler;
-		// Check who should handle the message - parent or child
-		switch(uMsg) {
-		case WM_CTLCOLORBTN:
-		case WM_CTLCOLORDLG:
-		case WM_CTLCOLOREDIT:
-		case WM_CTLCOLORLISTBOX:
-		case WM_CTLCOLORMSGBOX:
-		case WM_CTLCOLORSCROLLBAR:
-		case WM_CTLCOLORSTATIC: {
-			handler = reinterpret_cast<HWND>(lParam);
-		} break;
-		case WM_NOTIFY : {
-			NMHDR* nmhdr = reinterpret_cast<NMHDR*>(lParam);
+	if(handler != hwnd) {
+		w = hwnd_cast<Widget*>(hwnd);
+	}
+
+	// If this fails there's something wrong
+	dwtassert(w, "Expected to get a pointer to a widget - something's wrong");
+
+	if(!w) {
+		return returnUnknown(msg);
+	}
+
+	return w->getDispatcher().chain(msg);
+}
+
+Widget* WindowProc::getInitWidget(const MSG& msg) {
+	if(msg.message == WM_NCCREATE) {
+		return reinterpret_cast<Widget*>(reinterpret_cast<CREATESTRUCT*>(msg.lParam)->lpCreateParams);
+	}
+	return 0;
+}
+
+LRESULT WindowProc::returnUnknown(const MSG& msg) {
+	return ::DefWindowProc(msg.hwnd, msg.message, msg.wParam, msg.lParam);
+}
+
+HWND WindowProc::getHandler(const MSG& msg) {
+	HWND handler;
+
+	// Check who should handle the message - parent or child
+	switch(msg.message) {
+	case WM_CTLCOLORBTN:
+	case WM_CTLCOLORDLG:
+	case WM_CTLCOLOREDIT:
+	case WM_CTLCOLORLISTBOX:
+	case WM_CTLCOLORMSGBOX:
+	case WM_CTLCOLORSCROLLBAR:
+	case WM_CTLCOLORSTATIC:
+		{
+			handler = reinterpret_cast<HWND>(msg.lParam);
+			break;
+		}
+
+	case WM_NOTIFY :
+		{
+			NMHDR* nmhdr = reinterpret_cast<NMHDR*>(msg.lParam);
 			handler = nmhdr->hwndFrom;
-		} break;
-		case WM_COMMAND: {
-			if(lParam != 0) {
-				handler = reinterpret_cast<HWND>(lParam);
+			break;
+		}
+
+	case WM_COMMAND:
+		{
+			if(msg.lParam != 0) {
+				handler = reinterpret_cast<HWND>(msg.lParam);
 			} else {
-				handler = hwnd;
+				handler = msg.hwnd;
 			}
-		} break;
-		default: {
+			break;
+		}
+
+	default:
+		{
 			// By default, widgets handle their own messages
-			handler = hwnd;
-		}
-		}
-		return handler;
-	}
-};
-
-namespace Policies {
-/// Policy for modeless dialogs
-class Dialog
-{
-public:
-	Dialog() { }
-
-	static LRESULT returnDestroyed(HWND hWnd, UINT msg, WPARAM wPar, LPARAM lPar) {
-		return FALSE;
-	}
-
-	static LRESULT returnHandled(LRESULT, HWND, UINT, WPARAM, LPARAM) {
-		// A dialog Widget should return TRUE to the windows internal dialog
-		// message handler procedure to tell windows that it have handled the
-		// message
-		/// @todo We should SetWindowLong with the actual result in some cases, see DialogProc docs
-		return TRUE;
-	}
-
-	static LRESULT returnUnknown(HWND hWnd, UINT msg, WPARAM wPar, LPARAM lPar) {
-		return returnUnhandled(hWnd, msg, wPar, lPar);
-	}
-
-	static LRESULT returnUnhandled( HWND hWnd, UINT msg, WPARAM wPar, LPARAM lPar ) {
-		// As opposed to a "normal" Widget the dialog Widget should NOT return
-		// ::DefaultMessageProc or something similar since this is done internally
-		// INSIDE windows but rather return FALSE to indicate it DID NOT handle the
-		// message, exception is WM_INITDIALOG which must return TRUE if unhandled
-		// to set focus to default "focusable" control!
-		if ( WM_INITDIALOG == msg )
-			return TRUE;
-		return FALSE;
-	}
-
-	static void initPolicy(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-		if(uMsg == WM_INITDIALOG) {
-/*			// extracting the this pointer and stuffing it into the Window with SetProp
-			Widget* This = Widget::fromLParam(lParam);
-			This->setHandle( hwnd );*/
-		}
-	}
-};
-
-/// Aspect classes for a normal Container Widget
-/** Used as the third template argument to WidgetFactory if you're creating a normal
-  * Container Widget Note that this one is default so if you don't supply  policy
-  * SmartWin will assume this is the one you're after!
-  */
-struct Normal
-{
-	static LRESULT returnDestroyed(const MSG& msg) {
-		return ::DefWindowProc(msg.hwnd, msg.message, msg.wParam, msg.lParam);
-	}
-
-	static LRESULT returnHandled(LRESULT hres, const MSG& msg) {
-		return hres;
-	}
-
-	static LRESULT returnUnknown(MSG& msg) {
-		return ::DefWindowProc(msg.hwnd, msg.message, msg.wParam, msg.lParam);
-	}
-
-	static LRESULT returnUnhandled(Widget* w, const MSG& msg) {
-		return ::DefWindowProc(msg.hwnd, msg.message, msg.wParam, msg.lParam);
-	}
-
-	static Widget* getInitWidget(MSG& msg) {
-		if (msg.message == WM_NCCREATE) {
-			// extracting the this pointer and stuffing it into the Window with SetProp
-			CREATESTRUCT* cs = reinterpret_cast<CREATESTRUCT*>( msg.lParam );
-			return reinterpret_cast<Widget*>( cs->lpCreateParams );
-		}
-
-		return 0;
-	}
-};
-
-struct Chain : public Normal {
-	static LRESULT returnUnhandled(Widget* w, const MSG& msg) {
-		return w->getDispatcher().chain(msg);
-	}
-};
-
-/// Aspect classes for a MDI Child Container Widget
-struct MDIChild
-{
-	MDIChild(Widget* parent) { }
-
-	static LRESULT returnDestroyed(HWND hWnd, UINT msg, WPARAM wPar, LPARAM lPar) {
-		return ::DefMDIChildProc( hWnd, msg, wPar, lPar );
-	}
-
-	static LRESULT returnHandled(HRESULT hres, HWND hWnd, UINT msg, WPARAM wPar, LPARAM lPar )
-	{
-		switch ( msg )
-		{
-			case WM_SIZE :
-			{
-				return ::DefMDIChildProc( hWnd, msg, wPar, lPar );
-			}
-			default:
-				return hres;
+			handler = msg.hwnd;
+			break;
 		}
 	}
 
-	static LRESULT returnUnknown( HWND hWnd, UINT msg, WPARAM wPar, LPARAM lPar ) {
-		return returnUnhandled(hWnd, msg, wPar, lPar);
-	}
-
-	static LRESULT returnUnhandled( HWND hWnd, UINT msg, WPARAM wPar, LPARAM lPar )
-	{
-		return ::DefMDIChildProc( hWnd, msg, wPar, lPar );
-	}
-
-	// STATIC EXTRACTER/Windows Message Procedure, extracts the this pointer and
-	// dispatches the message to the this Widget
-	static void initPolicy( HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam )
-	{
-		if ( uMsg == WM_NCCREATE )
-		{
-			/*
-			// extracting the this pointer and stuffing it into the Window with SetProp
-			CREATESTRUCT * cs = reinterpret_cast< CREATESTRUCT * >( lParam );
-			MDICREATESTRUCT * mcs = reinterpret_cast< MDICREATESTRUCT*>(cs->lpCreateParams);
-
-			MDIChild* This = reinterpret_cast<MDIChild*>(mcs->lParam);
-			This->setHandle(hWnd);*/
-		}
-	}
-};
-
-template<typename WidgetType>
-struct MDIFrame : public Normal {
-	MDIFrame() { }
-
-	LRESULT returnUnhandled( HWND hWnd, UINT msg, WPARAM wPar, LPARAM lPar ) {
-		WidgetType* This = static_cast<WidgetType*>(this);
-		if(This->getMDIParent()) {
-			return ::DefFrameProc( hWnd, This->getMDIParent()->handle(), msg, wPar, lPar );
-		}
-		return Normal::returnUnhandled(hWnd, msg, wPar, lPar);
-	}
-
-};
-
+	return handler;
 }
 
-Dispatcher::Dispatcher(LPCTSTR className, WNDPROC initProc) : atom(0) {
-	WNDCLASSEX cls = makeWndClass(className, initProc);
+#ifdef PORT_ME /// @todo for MDI, make MDIChildProc derive from WindowProc and MDIFrameDispatcher from Dispatcher
 
-	atom = ::RegisterClassEx(&cls);
-	if(!atom) {
-		throw Win32Exception("Unable to register class");
+LRESULT MDIChildProc::returnUnknown(const MSG& msg) {
+	return ::DefMDIChildProc(msg.hwnd, msg.message, msg.wParam, msg.lParam);
+}
+
+Widget* MDIChildProc::getInitWidget(const MSG& msg) {
+	if(msg.message == WM_NCCREATE) {
+		return reinterpret_cast<Widget*>(reinterpret_cast<MDICREATESTRUCT*>(
+			reinterpret_cast<CREATESTRUCT*>(msg.lParam)->lpCreateParams)->lParam);
 	}
 }
+
+LRESULT MDIFrameDispatcher::chain(const MSG& msg) {
+	if(getMDIParent()) {
+		return ::DefFrameProc(hWnd, getMDIParent()->handle(), msg, wPar, lPar);
+	}
+	return NormalDispatcher::chain(msg);
+}
+
+#endif
 
 Dispatcher::Dispatcher(WNDCLASSEX& cls) : atom(0) {
 	atom = ::RegisterClassEx(&cls);
@@ -306,16 +186,20 @@ HBRUSH Dispatcher::getDefaultBackground() {
 	return background;
 }
 
-WNDCLASSEX Dispatcher::makeWndClass(LPCTSTR className, WNDPROC initProc) {
+WNDCLASSEX Dispatcher::makeWndClass(LPCTSTR className) {
 	WNDCLASSEX cls = { sizeof(WNDCLASSEX) };
+	fillWndClass(cls, className);
+	return cls;
+}
 
-	cls.lpfnWndProc = initProc ? initProc : WindowProc<Policies::Normal>::initProc;
+void Dispatcher::fillWndClass(WNDCLASSEX& cls, LPCTSTR className) {
+	cls.style = CS_DBLCLKS;
+	cls.lpfnWndProc = WindowProc::initProc;
 	cls.hInstance = ::GetModuleHandle(NULL);
-	cls.lpszClassName = className;
 	cls.hCursor = getDefaultCursor();
 	cls.hbrBackground = getDefaultBackground();
-
-	return cls;
+	cls.lpszMenuName = 0;
+	cls.lpszClassName = className;
 }
 
 NormalDispatcher::NormalDispatcher(WNDCLASSEX& cls) :
@@ -332,14 +216,9 @@ LRESULT NormalDispatcher::chain(const MSG& msg) {
 	return ::DefWindowProc(msg.hwnd, msg.message, msg.wParam, msg.lParam);
 }
 
-ChainingDispatcher::ChainingDispatcher(LPCTSTR className_, WNDPROC wndProc_) :
-	Dispatcher(className_, WindowProc<Policies::Chain>::initProc),
-	wndProc(wndProc_)
-{ }
-
 ChainingDispatcher::ChainingDispatcher(WNDCLASSEX& cls, WNDPROC wndProc_) :
-	Dispatcher(cls),
-	wndProc(wndProc_)
+Dispatcher(cls),
+wndProc(wndProc_)
 { }
 
 LRESULT ChainingDispatcher::chain(const MSG& msg) {
@@ -355,11 +234,7 @@ std::auto_ptr<Dispatcher> ChainingDispatcher::superClass(LPCTSTR original, LPCTS
 
 	WNDPROC proc = orgClass.lpfnWndProc;
 
-	orgClass.lpfnWndProc = WindowProc<Policies::Chain>::initProc;
-	orgClass.lpszClassName = newName;
-	orgClass.hInstance = ::GetModuleHandle(NULL);
-	orgClass.lpszMenuName = 0;
-	orgClass.style = CS_DBLCLKS;
+	fillWndClass(orgClass, newName);
 
 	return std::auto_ptr<Dispatcher>(new ChainingDispatcher(orgClass, proc));
 }
