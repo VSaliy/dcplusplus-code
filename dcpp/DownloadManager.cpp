e65717c71a6ebf2f89514b4e2f9faafb4eca5677
cologic <ne5@parsoma.net>
2010-04-23 14:40:39 -0400

    see bug #550300, this is http://launchpadlibrarian.net/42307173/tthcorruption_bm.patch

--- dcpp/DownloadManager.cpp
+++ dcpp/DownloadManager.cpp
@@ -326,6 +326,7 @@ void DownloadManager::endData(UserConnection* aSource) {
 		try {
 			d->getFile()->flush();
 		} catch(const FileException& e) {
+			d->resetPos();
 			failDownload(aSource, e.getError());
 			return;
 		}
--- dcpp/Transfer.h
+++ dcpp/Transfer.h
@@ -50,6 +50,8 @@ public:
 
 	int64_t getStartPos() const { return getSegment().getStart(); }
 
+	void resetPos() { pos = getStartPos(); }
+
 	void addPos(int64_t aBytes, int64_t aActual) { pos += aBytes; actual+= aActual; }
 
 	enum { MIN_SAMPLES = 15, MIN_SECS = 15 };
