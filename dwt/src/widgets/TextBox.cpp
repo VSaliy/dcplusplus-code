a69cffed9e5e3d28adc2f87069d0a295bad183c4
poy <poy@123gen.com>
2010-09-27 22:29:29 +0200

    fix ctrl+A

--- dwt/src/widgets/TextBox.cpp
+++ dwt/src/widgets/TextBox.cpp
@@ -77,7 +77,7 @@ void TextBox::create(const Seed& cs) {
 
 	// multiline text-boxes don't handle ctrl + A so we have do it ourselves...
 	if((cs.style & ES_MULTILINE) == ES_MULTILINE)
-		onKeyDown([this](bool c) { return handleKeyDown(c); });
+		onKeyDown([this](int c) { return handleKeyDown(c); });
 }
 
 void TextBox::setText(const tstring& txt) {
