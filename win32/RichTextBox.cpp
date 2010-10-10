664d3df7a3143e8f1f2bd71b1d42ea12312127bc
eMTee <emtee11@gmail.com>
2010-10-10 11:26:48 +0200

    Fix compile with mingw

--- win32/RichTextBox.cpp
+++ win32/RichTextBox.cpp
@@ -62,7 +62,7 @@ MenuPtr RichTextBox::getMenu() {
 	MenuPtr menu = BaseType::getMenu();
 
 	menu->appendSeparator();
-	menu->appendItem(T_("&Find...\tF3"), [this]() { findText(findTextPopup()); }, dwt::IconPtr(), !getText().empty());
+	menu->appendItem(T_("&Find...\tF3"), [this]() { this->findText(this->findTextPopup()); }, dwt::IconPtr(), !getText().empty());
 
 	return menu;
 }
