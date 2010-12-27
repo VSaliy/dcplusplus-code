5cb18d211a46395b1d59b315df4f8156245bf6a0
poy <poy@123gen.com>
2010-12-27 17:33:30 +0100

    log ports UPnP closes

--- dcpp/UPnPManager.cpp
+++ dcpp/UPnPManager.cpp
@@ -121,8 +121,9 @@ int UPnPManager::run() {
 }
 
 void UPnPManager::close(UPnP& impl) {
-	if(!impl.close()) {
-		log(_("Failed to remove port mappings"));
+	if(impl.hasRules()) {
+		log(impl.close() ? str(F_("Successfully removed port mappings with the %1% interface") % impl.getName()) :
+			str(F_("Failed to remove port mappings with the %1% interface") % impl.getName()));
 	}
 }
 
