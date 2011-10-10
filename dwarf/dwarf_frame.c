4b6e63076be6f81d7db66b4b556e1920ca4d60d9
poy <poy@123gen.com>
2011-10-10 22:34:10 +0200

    update libdwarf

--- dwarf/dwarf_frame.c
+++ dwarf/dwarf_frame.c
@@ -2317,9 +2317,17 @@ dwarf_set_frame_undefined_value(Dwarf_Debug dbg, Dwarf_Half value)
     return orig;
 }
 
-
-
-
+/*  Does something only if value passed in is greater than 0 and
+    a size than we can handle (in number of bytes).  */
+Dwarf_Small dwarf_set_default_address_size(Dwarf_Debug dbg,
+    Dwarf_Small value  )
+{
+    Dwarf_Small orig = dbg->de_pointer_size;
+    if (value > 0 && value <= sizeof(Dwarf_Addr)) {
+        dbg->de_pointer_size = value;
+    }
+    return orig;
+}
 
 static int 
 init_reg_rules_alloc(Dwarf_Debug dbg,struct Dwarf_Frame_s *f,
--- win32/MainWindow.cpp
+++ win32/MainWindow.cpp
@@ -1319,7 +1319,7 @@ void MainWindow::checkGeoUpdate(bool v6) {
 	// update when the database is non-existent or older than 16 days (GeoIP updates every month).
 	try {
 		File f(GeoManager::getDbPath(v6) + ".gz", File::READ, File::OPEN);
-		if(f.getSize() > 0 && f.getLastModified() > GET_TIME() - 3600 * 24 * 16) {
+		if(f.getSize() > 0 && static_cast<time_t>(f.getLastModified()) > GET_TIME() - 3600 * 24 * 16) {
 			return;
 		}
 	} catch(const FileException&) { }
