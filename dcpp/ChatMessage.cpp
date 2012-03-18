7bc72e3e0b6f6729f6cd09659c9245877d64e5a1
poy <poy@123gen.com>
2012-03-18 16:21:56 +0100

    Format chat links

--- dcpp/ChatMessage.cpp
+++ dcpp/ChatMessage.cpp
@@ -26,7 +26,13 @@
 #include "SimpleXML.h"
 #include "Util.h"
 
+#include <boost/range/adaptor/reversed.hpp>
+
 namespace dcpp {
+
+using std::make_pair;
+using std::map;
+using std::pair;
 
 ChatMessage::ChatMessage(const string& text, const OnlineUser* from,
 	const OnlineUser* to, const OnlineUser* replyTo,
@@ -91,7 +97,101 @@ messageTimestamp(messageTimestamp)
 	}
 
 	message += tmp;
-	htmlMessage += addSpan("text", tmp, Util::emptyString) + "</span>";
+
+	/* format the message; this will involve adding custom tags. this table holds the tags to be
+	added along with their position. */
+	struct Tag { string s; bool opening; size_t otherTag; };
+	map<size_t, Tag> tags;
+
+	/* link formatting - optimize the lookup a bit by using the fact that every link identifier
+	(except www ones) contains a colon. */
+	/// @todo add support for spaces within links enclosed by brackets / quotes (see URI RFC)
+	/// @todo friendly magnet links
+	auto addLink = [&tmp, &xmlTmp, &tags](size_t begin, size_t end) {
+		Tag openingTag = { "<a href=\"" + SimpleXML::escape(tmp.substr(begin, end - begin), xmlTmp, true) + "\">", true, end },
+			closingTag = { "</a>", false, begin };
+		tags.emplace(begin, std::move(openingTag));
+		tags.emplace(end, std::move(closingTag));
+	};
+
+	static const string delimiters = " \t\r\n<>\"";
+
+	i = 0;
+	size_t begin, end;
+	const auto n = tmp.size();
+	while((i = tmp.find(':', i)) != string::npos) {
+
+		if((begin = tmp.find_last_of(delimiters, i)) == string::npos) begin = 0; else ++begin;
+		if((end = tmp.find_first_of(delimiters, i + 1)) == string::npos) end = n;
+
+		if(i > 0 &&
+			(i + 4 < n && tmp[i + 1] == '/' && tmp[i + 2] == '/') || // "http://", etc
+			(i >= begin + 6 && !tmp.compare(begin, 6, "magnet")) ||
+			(i >= begin + 6 && !tmp.compare(begin, 6, "mailto")))
+		{
+			addLink(begin, end);
+			i = end;
+
+		} else {
+			++i;
+		}
+	}
+
+	// check for www links.
+	i = 0;
+	while((i = tmp.find("www.", i)) != string::npos) {
+		if(i + 5 <= n && (i == 0 || delimiters.find(tmp[i - 1]) != string::npos)) {
+			if((end = tmp.find_first_of(delimiters, i + 4)) == string::npos) end = n;
+			if(i + 5 <= end) {
+				addLink(i, end);
+				i = end;
+			}
+		}
+		i += 5;
+	}
+
+	htmlMessage += "<span id=\"text\">";
+
+	/* write tags and the message data. support entangled tags, such as:
+	<a> <b> </a> </b> -> <a> <b> </b></a><b> </b> */
+
+	size_t pos = 0;
+	vector<size_t> openTags;
+
+	for(auto& tag: tags) {
+		htmlMessage += SimpleXML::escape(tmp.substr(pos, tag.first - pos), xmlTmp, false);
+		pos = tag.first;
+
+		if(tag.second.opening) {
+			htmlMessage += tag.second.s;
+			openTags.push_back(tag.first);
+
+		} else {
+			if(openTags.back() == tag.second.otherTag) {
+				// common case: no entangled tag; just write the closing tag.
+				htmlMessage += tag.second.s;
+				openTags.pop_back();
+
+			} else {
+				// there are entangled tags: write closing & opening tags of currently open tags.
+				for(auto key: openTags | boost::adaptors::reversed) { if(key >= tag.second.otherTag) {
+					htmlMessage += tags[tags[key].otherTag].s;
+				} }
+				openTags.erase(remove(openTags.begin(), openTags.end(), tag.second.otherTag), openTags.end());
+				for(auto key: openTags) { if(key >= tag.second.otherTag) {
+					htmlMessage += tags[key].s;
+				} }
+			}
+		}
+	}
+
+	if(pos != n) {
+		htmlMessage += SimpleXML::escape(tmp.substr(pos), xmlTmp, false);
+	}
+
+	htmlMessage += "</span></span>";
+
+	dcdebug("html: %s\n", htmlMessage.c_str());
 
 	/// @todo send this to plugins
 }
