f22a38a3fa035528c76d31d4bcdec7259aa293be
poy <poy@123gen.com>
2012-06-29 21:05:29 +0200

    correct splitter position checking; add a splitter test

---
+++ dwt/test/SplitTest.cpp
@@ -0,0 +1,27 @@
+#include <dwt/widgets/Window.h>
+#include <dwt/widgets/Label.h>
+#include <dwt/widgets/SplitterContainer.h>
+#include <dwt/Texts.h>
+
+namespace dwt {
+tstring Texts::get(Text text) { return _T("test"); }
+}
+
+int dwtMain(dwt::Application& app)
+{
+	auto window = new dwt::Window();
+	window->create();
+	window->onClosing([] { return ::PostQuitMessage(0), true; });
+
+	auto split = window->addChild(dwt::SplitterContainer::Seed(0.3, true));
+
+	split->addChild(dwt::Label::Seed(_T("First row")));
+	split->addChild(dwt::Label::Seed(_T("Second row")));
+	split->addChild(dwt::Label::Seed(_T("Third row")));
+
+	split->resize(dwt::Rectangle(window->getClientSize()));
+
+	app.run();
+
+	return 0;
+}
