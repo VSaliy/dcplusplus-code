d51e90d9f7dca74635ae390ce7af7c7377986c3f
poy <poy@123gen.com>
2012-07-20 15:49:53 +0200

    add filtering settings to the dev plugin

--- plugins/Dev/resource.h
+++ plugins/Dev/resource.h
@@ -8,6 +8,9 @@
 #define IDC_COPY                        1002
 #define IDC_CLEAR                       1003
 #define IDC_SCROLL                      1004
+#define IDC_HUB_MESSAGES                1005
+#define IDC_USER_MESSAGES               1006
+#define IDC_FILTER                      1007
 
 // Next default values for new objects
 // 
@@ -15,7 +18,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        102
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_CONTROL_VALUE         1008
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
