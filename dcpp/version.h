54bd54dcd158aa13bdc27abbc4ef4115c03659ce
iceman50 <bdcdevel@gmail.com>
2012-10-03 16:01:13 -0500

    bump version to 0.801

--- dcpp/version.h
+++ dcpp/version.h
@@ -21,8 +21,8 @@
 #define APPNAME "DC++"
 
 // don't forget to update the .rc file as well!
-#define VERSIONSTRING "0.800"
-#define VERSIONFLOAT 0.800
+#define VERSIONSTRING "0.801"
+#define VERSIONFLOAT 0.801
 
 namespace dcpp {
 extern const std::string fullVersionString;
