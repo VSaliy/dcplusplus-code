80352ca46faa56977fed8ffa845a15c71930bf0a
poy <poy@123gen.com>
2013-12-06 21:58:25 +0100

    re-apply rev 2211.1.3

--- dcpp/Speaker.h
+++ dcpp/Speaker.h
@@ -39,68 +39,12 @@ public:
 	Speaker() noexcept { }
 	virtual ~Speaker() { }
 
-	/// @todo simplify when we have variadic templates
-
-	template<typename T0>
-	void fire(T0&& type) noexcept {
+	template<typename... T>
+	void fire(T&&... type) noexcept {
 		Lock l(listenerCS);
 		tmp = listeners;
 		for(auto i: tmp) {
-			i->on(forward<T0>(type));
-		}
-	}
-
-	template<typename T0, typename T1>
-	void fire(T0&& type, T1&& p1) noexcept {
-		Lock l(listenerCS);
-		tmp = listeners;
-		for(auto i: tmp) {
-			i->on(forward<T0>(type), forward<T1>(p1));
-		}
-	}
-
-	template<typename T0, typename T1, typename T2>
-	void fire(T0&& type, T1&& p1, T2&& p2) noexcept {
-		Lock l(listenerCS);
-		tmp = listeners;
-		for(auto i: tmp) {
-			i->on(forward<T0>(type), forward<T1>(p1), forward<T2>(p2));
-		}
-	}
-
-	template<typename T0, typename T1, typename T2, typename T3>
-	void fire(T0&& type, T1&& p1, T2&& p2, T3&& p3) noexcept {
-		Lock l(listenerCS);
-		tmp = listeners;
-		for(auto i: tmp) {
-			i->on(forward<T0>(type), forward<T1>(p1), forward<T2>(p2), forward<T3>(p3));
-		}
-	}
-
-	template<typename T0, typename T1, typename T2, typename T3, typename T4>
-	void fire(T0&& type, T1&& p1, T2&& p2, T3&& p3, T4&& p4) noexcept {
-		Lock l(listenerCS);
-		tmp = listeners;
-		for(auto i: tmp) {
-			i->on(forward<T0>(type), forward<T1>(p1), forward<T2>(p2), forward<T3>(p3), forward<T4>(p4));
-		}
-	}
-
-	template<typename T0, typename T1, typename T2, typename T3, typename T4, typename T5>
-	void fire(T0&& type, T1&& p1, T2&& p2, T3&& p3, T4&& p4, T5&& p5) noexcept {
-		Lock l(listenerCS);
-		tmp = listeners;
-		for(auto i: tmp) {
-			i->on(forward<T0>(type), forward<T1>(p1), forward<T2>(p2), forward<T3>(p3), forward<T4>(p4), forward<T5>(p5));
-		}
-	}
-
-	template<typename T0, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6>
-	void fire(T0&& type, T1&& p1, T2&& p2, T3&& p3, T4&& p4, T5&& p5, T6&& p6) noexcept {
-		Lock l(listenerCS);
-		tmp = listeners;
-		for(auto i: tmp) {
-			i->on(forward<T0>(type), forward<T1>(p1), forward<T2>(p2), forward<T3>(p3), forward<T4>(p4), forward<T5>(p5), forward<T6>(p6));
+			i->on(forward<T>(type)...);
 		}
 	}
 
