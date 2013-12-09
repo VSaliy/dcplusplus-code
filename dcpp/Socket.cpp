fd55b54c3001ff35392ca3d531054a391df9285c
poy <poy@123gen.com>
2013-12-10 00:12:01 +0100

    Correct connecting to IPv6 addresses

--- dcpp/Socket.cpp
+++ dcpp/Socket.cpp
@@ -344,22 +344,31 @@ void Socket::connect(const string& aAddr, const string& aPort, const string& loc
 	// We try to connect to both IPv4 and IPv6 if available
 	auto addr = resolveAddr(aAddr, aPort);
 
+	string lastError;
 	for(auto ai = addr.get(); ai; ai = ai->ai_next) {
 		if((ai->ai_family == AF_INET && !sock4.valid()) ||
 			(ai->ai_family == AF_INET6 && !sock6.valid() && !v4only))
 		{
-			auto sock = create(*ai);
-			auto &localIp = ai->ai_family == AF_INET ? getLocalIp4() : getLocalIp6();
+			try {
+				auto sock = create(*ai);
+				auto &localIp = ai->ai_family == AF_INET ? getLocalIp4() : getLocalIp6();
 
-			if(!localPort.empty() || !localIp.empty()) {
-				auto local = resolveAddr(localIp, localPort, ai->ai_family);
-				check([&] { return ::bind(sock, local->ai_addr, local->ai_addrlen); });
+				if(!localPort.empty() || !localIp.empty()) {
+					auto local = resolveAddr(localIp, localPort, ai->ai_family);
+					check([&] { return ::bind(sock, local->ai_addr, local->ai_addrlen); });
+				}
+
+				check([&] { return ::connect(sock, ai->ai_addr, ai->ai_addrlen); }, true);
+				setIp(resolveName(ai->ai_addr, ai->ai_addrlen));
+			} catch(const SocketException& e) {
+				ai->ai_family == AF_INET ? sock4.reset() : sock6.reset();
+				lastError = e.getError();
 			}
-
-			check([&] { return ::connect(sock, ai->ai_addr, ai->ai_addrlen); }, true);
-			setIp(resolveName(ai->ai_addr, ai->ai_addrlen));
 		}
 	}
+
+	if(!lastError.empty())
+		throw SocketException(lastError);
 }
 
 namespace {
