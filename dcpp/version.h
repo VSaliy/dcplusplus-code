7f8eb6c2d84e9c354941a161aac4a98505eeb6e1
poy <poy@123gen.com>
2014-04-06 22:52:06 +0200

    0.841

--- dcpp/version.h
+++ dcpp/version.h
@@ -21,8 +21,8 @@
 #define APPNAME "DC++"
 
 // don't forget to update the .rc file as well!
-#define VERSIONSTRING "0.840"
-#define VERSIONFLOAT 0.840
+#define VERSIONSTRING "0.841"
+#define VERSIONFLOAT 0.841
 
 namespace dcpp {
 extern const std::string fullVersionString;
