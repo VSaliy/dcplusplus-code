a5c744042432a0700df601371cef3651cf001f8f
cologic <cologic@users.sourceforge.net>
2014-05-18 03:12:03 -0400

    specify reasonable TLS ciphersuites

--- dcpp/CryptoManager.cpp
+++ dcpp/CryptoManager.cpp
@@ -69,8 +69,11 @@ CryptoManager::CryptoManager()
 		for(int i = KEY_RSA_512; i != KEY_LAST; ++i)
 			tmpKeysMap[i] = getTmpRSA(getKeyLength(static_cast<TLSTmpKeys>(i)));
 
+		const char ciphersuites[] = "ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:DHE-RSA-AES128-SHA:AES128-SHA";
 		SSL_CTX_set_options(clientContext, SSL_OP_NO_SSLv2 | SSL_OP_NO_SSLv3);
+		SSL_CTX_set_cipher_list(clientContext, ciphersuites);
 		SSL_CTX_set_options(serverContext, SSL_OP_SINGLE_DH_USE | SSL_OP_NO_SSLv2 | SSL_OP_NO_SSLv3);
+		SSL_CTX_set_cipher_list(serverContext, ciphersuites);
 
 		EC_KEY* tmp_ecdh;
 		if ((tmp_ecdh = EC_KEY_new_by_curve_name(NID_X9_62_prime256v1)) != NULL) {
