c3b9c5ac53fd89644cf7696155ef13379814bdca
poy <poy@123gen.com>
2015-03-22 13:31:23 +0100

    fix the version info getter

--- dcpp/gen_rev_id.py
+++ dcpp/gen_rev_id.py
@@ -1,7 +1,18 @@
 def get_rev_id(env):
+    """Attempt to get information about the repository, via the "hg log"
+    command. Its output is formatted via the "-T" parameter (see "hg templates"
+    for details).
+
+    :return: Version information string, or "[unknown]" on failure.
+    :rtype: str.
+    """
+
     try:
         import subprocess
-        ret = subprocess.check_output('hg tip -T \"r{short(node)}, {date(date, \'%Y.%m.%d %H:%M\')}\"', shell=True)
+        ret = subprocess.check_output(
+            'hg log -r tip -T "{node | short} - {date | isodate}"',
+            shell=True
+        )
         if ret:
             return ret
     except:
