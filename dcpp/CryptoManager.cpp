6fa0546e9c3d3903a0db9a85033964df9a94da5b
cologic <cologic@users.sourceforge.net>
2015-09-12 09:08:55 -0700

    add ECDHE-ECDSA-AES128-GCM-SHA256 and remove AES128-SHA TLS ciphersuite support

--- dcpp/CryptoManager.cpp
+++ dcpp/CryptoManager.cpp
@@ -69,7 +69,7 @@ CryptoManager::CryptoManager()
 		for(int i = KEY_RSA_2048; i != KEY_LAST; ++i)
 			tmpKeysMap[i] = getTmpRSA(getKeyLength(static_cast<TLSTmpKeys>(i)));
 
-		const char ciphersuites[] = "ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:DHE-RSA-AES128-SHA:AES128-SHA";
+		const char ciphersuites[] = "ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:DHE-RSA-AES128-SHA";
 		SSL_CTX_set_options(clientContext, SSL_OP_NO_SSLv2 | SSL_OP_NO_SSLv3 | SSL_OP_NO_COMPRESSION);
 		SSL_CTX_set_cipher_list(clientContext, ciphersuites);
 		SSL_CTX_set1_curves_list(clientContext, "P-256");
