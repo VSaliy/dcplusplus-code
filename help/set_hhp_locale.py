bc6b1c8e85a4f2bd2073c7bc0ec6da08b10a52eb
eMTee <emtee11@gmail.com>
2020-05-25 18:52:13 +0200

    Fixes for distro to build under SCons 3.x

--- help/set_hhp_locale.py
+++ help/set_hhp_locale.py
@@ -1,6 +1,6 @@
 def set_hhp_locale(target, lcid):
     print ('Setting ' + target + ' to the 0x%X locale' % lcid)
-    f = open(target, 'r+b')
+    f = open(target, 'r+')
     contents = f.read()
     f.seek(0)
     f.write(contents.replace('Language=0x409', 'Language=0x%X' % lcid))
