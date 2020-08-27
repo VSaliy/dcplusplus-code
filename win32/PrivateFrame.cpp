717df0991d03d59cc41047f5d59a443a515f404a
eMTee <emtee11@gmail.com>
2020-08-27 15:26:41 +0200

    Allow CCPM on unencrypted ADC hubs

--- win32/PrivateFrame.cpp
+++ win32/PrivateFrame.cpp
@@ -312,7 +312,6 @@ void PrivateFrame::startCC(bool silent) {
 		}
 
 		tstring err = ou->getUser()->isNMDC() ? T_("A secure ADC hub is required; this feature is not supported on NMDC hubs") :
-			!ou->getClient().isSecure() ? T_("The connection to the ADC hub used to initiate the channel must be encrypted") :
 			!ou->getIdentity().supports(AdcHub::CCPM_FEATURE) ? T_("The user does not support the CCPM ADC extension") : _T("");
 		if(!err.empty()) {
 			if(!silent) { addStatus(str(TF_("Cannot start the direct encrypted channel: %1%") % err)); }
